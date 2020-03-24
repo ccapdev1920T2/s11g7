cc875948198f17be0f1477f40f6d7fb3fd1dba48
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2020-03-24 17:35:21 +0800

    added admin view routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import studentLogin from '../views/student-login.vue'
+import studentRegister from '../views/student-register.vue'
 import studentSearch from '../views/student-search.vue'
 import studentManage from '../views/student-manage.vue'
 import studentResults from '../views/student-results.vue'
-import loginStudent from '../views/student-login.vue'
-import registerStudent from '../views/student-register.vue'
+
+import adminLogin from '../views/admin-login.vue'
+import adminManage from '../views/admin-manage-courses.vue'
+import adminAdd from '../views/admin-add-course.vue'
+import adminEdit from '../views/admin-edit-course.vue'
+
+
+
 
 
 Vue.use(VueRouter)
 
 const routes = [
+  // student routes
   {
     path: '/',
     name: 'loginStudent',
-    component: loginStudent
+    component: studentLogin
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: studentRegister
   },
   {
     path: '/search',
@@ -31,11 +45,29 @@ const routes = [
     name: 'Manage',
     component: studentManage
   },
+
+  // admin routes
   {
-    path: '/register',
-    name: 'Register',
-    component: registerStudent
-  }
+    path: '/admin',
+    name: 'loginAdmin',
+    component: adminLogin
+  },
+  {
+    path: '/admin-add',
+    name: 'adminAdd',
+    component: adminAdd
+  },
+  {
+    path: '/admin-edit',
+    name: 'adminEdit',
+    component: adminEdit
+  },
+  {
+    path: '/admin-manage',
+    name: 'adminManage',
+    component: adminManage
+  },
+  
     
   // {
   //   path: '/about',
