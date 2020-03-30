b8d7d4defcc24b48c41582c7d0262f7127091588
Lance Ricco Teng <47263399+S0rbetes@users.noreply.github.com>
2020-03-30 18:30:01 +0800

    Added validation for classtime

--- src/models/ClassTime.js
+++ src/models/ClassTime.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose'
 
 let ClassTimeSchema = mongoose.Schema({
+    /* If repeating = true, then timeslot follow /[MTWHFS]+/ regex format (e.g. "MW", "TH"),
+       else must be a date with the format MM/DD */
     timeslot: {
         type: String,
-        required: true
+        required: true,
+        validate: function(v) {
+            return /^[MTWHFS]+$|^\d{1,2}\/\d{1,2}$/.test(v)
+        }
     },
     room: {
         type: String,
