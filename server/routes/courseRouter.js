9f6078ca91b849746fbc08ed1c39d6c9e1644560
Lance Ricco Teng <47263399+S0rbetes@users.noreply.github.com>
2020-04-03 21:43:19 +0800

    Implemented add course and edit course

--- server/routes/courseRouter.js
+++ server/routes/courseRouter.js
@@ -7,26 +7,29 @@ const courseRouter = express.Router()
  */
 courseRouter.route('/')
     .get(courseController.getAllCourses)
+    .post(courseController.addCourse)
 
 /**
  * Use a GET request on http://localhost:5656/code/:courseCode/ to retrieve 
  * all courses that match the given courseCode. (e.g. CCAPDEV)
  */
-courseRouter.route('/code/:courseCode/')
+courseRouter.route('/code/:courseCode')
     .get(courseController.getCoursesByCode)
 
 /**
  * Use a GET request on http://localhost:/5656/code/:courseNum to retrieve
  * the specific course that matches the given courseNum
  */
-courseRouter.route('/number/:courseNum/')
+courseRouter.route('/number/:courseNum')
     .get(courseController.getCoursesByNumber)
+    .patch(courseController.editCourse)
+    .delete(courseController.deleteCourse)
 
 /**
  * Use a GET request on http://localhost:/5656/code/:courseNum to retrieve
- * the all STUDENTS who are a part of the course.
+ * all STUDENTS who are a part of the course.
  */
-courseRouter.route('/number/:courseNum/students/')
+courseRouter.route('/number/:courseNum/students')
     .get(courseController.getClassList)
 
 module.exports = courseRouter
