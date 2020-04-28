35d0a3198cfef98cd41e7410ca342728410146c3
Jason Evangelista <jase_evangelista@dlsu.edu.ph>
2020-04-28 20:47:17 +0800

    create session (partial)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
+
 const app = express()
 const port = process.env.PORT || 5656
 
@@ -21,8 +24,24 @@ app.use(function(req, res, next) {
   });
 
 // Middleware
+app.use(session({
+  'secret': 'ccapdev-session',
+  'resave': false,
+  'saveUninitialized': false,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// app.use(function (req, res) {
+
+//   var details = {};
+
+//   if(req.session.idnum) {
+//       details.idnum = req.session.idnum;
+//   }
+//   // render error
+// });
 
 // Connect Routers
 app.use('/api/students', studentRouter)
